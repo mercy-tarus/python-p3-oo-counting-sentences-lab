d9df60bde944a7d3d2a732c945f51f4160b15714
mercy-tarus <merciechelah1@gmail.com>
2024-06-06 22:46:31 +0300

    update

--- lib/count_sentences.py
+++ lib/count_sentences.py
@@ -1,4 +1,27 @@
 #!/usr/bin/env python3
 
+import io
+import sys
+
 class MyString:
+  def __init__(self, value=''):
+        if not isinstance(value, str):
+            raise TypeError("The value must be a string.\n")
+        self.value = value
+
+  def is_sentence(self):
+        return self.value.endswith('.')
+
+  def is_question(self):
+        return self.value.endswith('?')
+
+  def is_exclamation(self):
+        return self.value.endswith('!')
+
+  def count_sentences(self):
+        
+        temp_value = self.value.replace('.', '|').replace('?', '|').replace('!', '|')
+        
+        sentences = [sentence for sentence in temp_value.split('|') if sentence.strip()]
+        return len(sentences)
   pass
